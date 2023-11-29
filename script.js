9c4d29e7a1aa71f3bc9109bdf40a47849121a946
Ojas Pratap Singh <128834924+Ojas-Pratap-Singh@users.noreply.github.com>
2023-11-29 18:14:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let pTag = document.querySelector("status");
+let button = document.querySelector("enterBtn");
+button.addEventListener('click',()=>{
+
+	pTag.innerHTML='<h1>Entered Metaverse</h1>';
+});
+
